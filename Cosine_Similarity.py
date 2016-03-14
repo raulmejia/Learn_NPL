ca9466a91dcb9f8aa2d9c17af923652bc4643b39
raulmejia <raulmejia@users.noreply.github.com>
2016-03-13 19:06:56 -0500

    Update Cosine_Similarity.py

--- Cosine_Similarity.py
+++ Cosine_Similarity.py
@@ -1,3 +1,20 @@
+import argparse
+from pprint import pprint
+
+parser = argparse.ArgumentParser(description='cos classify some files')
+parser.add_argument('--archivos', type=argparse.FileType('r'), required=False, nargs='*')
+args = parser.parse_args()
+
+# args tiene una lista de archivos abiertos! checa:
+pprint(args.archivos)
+
+# puedes hacer algo como:
+for archivo in args.archivos:
+    pprint(archivo.read()[0:70])
+
+
+
+
 
 documents = (
 "The sky is blue",
