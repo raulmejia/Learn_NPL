ed868b1ee86081416f70798b6888fa0889c2e450
raulmejia <raulmejia@users.noreply.github.com>
2016-03-13 19:04:56 -0500

    begin

--- Cosine_Similarity.py
+++ Cosine_Similarity.py
@@ -1,1 +1,16 @@
 
+documents = (
+"The sky is blue",
+"The sun is bright",
+"The sun in the sky is bright",
+"We can see the shining sun, the bright sun"
+)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+print tfidf_matrix.shape
+
+from sklearn.metrics.pairwise import cosine_similarity
+cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
+array([[ 1.        ,  0.36651513,  0.52305744,  0.13448867]])
